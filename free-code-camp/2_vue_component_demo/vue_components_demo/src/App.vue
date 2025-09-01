21f52c036e6d5c5a009a3b7bef8355cd537eb587
shohel <gsshohel1314@gmail.com>
2025-09-01 13:32:54 +0600

    Local and global component setup

--- free-code-camp/2_vue_component_demo/vue_components_demo/src/App.vue
+++ free-code-camp/2_vue_component_demo/vue_components_demo/src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import ContactUs from '@/components/ContactUs.vue';
+import ButtonCounter from '@/components/ButtonCounter.vue';
+
 const message = "Hello from Vue"
 </script>
 
 <template>
     <h1>{{ message }}</h1>
+
+    <ContactUs />
+    <ButtonCounter />
 </template>
---
+++ free-code-camp/2_vue_component_demo/vue_components_demo/src/components/ContactUs.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const email = ref("admin@gmail.com");
+</script>
+
+<template>
+    <div>
+        <h1>Contact Us</h1>
+        <p>Please send an email to "{{ email }}"</p>
+    </div>
+</template>
